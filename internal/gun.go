9e511c2bf4aa7b97448a379183e184028ec04fbb
Fredrik Leijon <fredrik.leijon@gmail.com>
2021-12-17 12:48:33 +0100

    day17, bruteforce

---
+++ internal/gun.go
@@ -0,0 +1,135 @@
+package aoc
+
+import (
+	"strconv"
+	"strings"
+)
+
+type Target struct {
+	x0, y0, x1, y1 int
+}
+
+type Velocity struct {
+	x, y int
+}
+
+type Position struct {
+	x, y int
+	maxY int
+}
+
+func ParseTargetArea(input string) Target {
+	res := Target{}
+	parts := strings.Split(input, "=")
+
+	xparts := strings.Split(parts[1], "..")
+
+	v, _ := strconv.Atoi(xparts[0])
+	res.x0 = v
+
+	x2 := strings.Split(xparts[1], ",")
+	v, _ = strconv.Atoi(x2[0])
+	res.x1 = v
+
+	yparts := strings.Split(parts[2], "..")
+
+	v, _ = strconv.Atoi(yparts[0])
+	res.y1 = v
+
+	v, _ = strconv.Atoi(yparts[1])
+	res.y0 = v
+
+	return res
+}
+
+func UpdateVelocity(velocity Velocity) Velocity {
+	newvelocity := Velocity{}
+	newvelocity.y = velocity.y - 1
+
+	if velocity.x > 0 {
+		newvelocity.x = velocity.x - 1
+	}
+	if velocity.x < 0 {
+		newvelocity.x = velocity.x + 1
+	}
+
+	return newvelocity
+}
+
+func ApplyVelocity(v Velocity, p Position) Position {
+	newPos := Position{}
+	newPos.x = p.x + v.x
+	newPos.y = p.y + v.y
+	if newPos.y > p.y {
+		newPos.maxY = newPos.y
+	} else {
+		newPos.maxY = p.maxY
+	}
+
+	return newPos
+}
+
+func InTargetArea(p Position, t Target) bool {
+	if t.x0 <= p.x && p.x <= t.x1 {
+		if t.y0 >= p.y && p.y >= t.y1 {
+			return true
+		}
+	}
+
+	return false
+}
+
+func PassedTargetArea(p Position, v Velocity, t Target) bool {
+	if p.x > t.x1 || p.y < t.y1 {
+		return true
+	}
+	return false
+}
+
+func StuckX(p Position, v Velocity, t Target) bool {
+	if v.x == 0 {
+		if !(t.x0 <= p.x && p.x <= t.x1) {
+			return true
+		}
+	}
+
+	return false
+}
+
+func WillHit(pos Position, v Velocity, target Target) (bool, Position) {
+	hit := false
+	for {
+		if StuckX(pos, v, target) {
+			break
+		}
+		if PassedTargetArea(pos, v, target) {
+			break
+		}
+
+		if InTargetArea(pos, target) {
+			hit = true
+			break
+		}
+
+		pos = ApplyVelocity(v, pos)
+		v = UpdateVelocity(v)
+	}
+	return hit, pos
+}
+
+func FindMaxY(target Target) int {
+	bestPos := Position{}
+
+	for i := 1; i < 400; i++ {
+		for j := 0; j < 400; j++ {
+			pos := Position{}
+			vel := Velocity{i, j}
+			hit, endPos := WillHit(pos, vel, target)
+			if hit && endPos.maxY > bestPos.maxY {
+				bestPos = endPos
+			}
+		}
+	}
+
+	return bestPos.maxY
+}
---
+++ internal/gun_test.go
@@ -0,0 +1,81 @@
+package aoc
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func Test_ParseExampleInput(t *testing.T) {
+	target := ParseTargetArea("target area: x=20..30, y=-10..-5")
+
+	assert.Equal(t, 20, target.x0)
+	assert.Equal(t, 30, target.x1)
+	assert.Equal(t, -5, target.y0)
+	assert.Equal(t, -10, target.y1)
+}
+
+func Test_UpdateVelocity(t *testing.T) {
+	v := Velocity{7, 2}
+	pos := Position{}
+
+	pos = ApplyVelocity(v, pos)
+	assert.Equal(t, 7, pos.x)
+	assert.Equal(t, 2, pos.y)
+
+	v = UpdateVelocity(v)
+	assert.Equal(t, 6, v.x)
+	assert.Equal(t, 1, v.y)
+
+	pos = ApplyVelocity(v, pos)
+	assert.Equal(t, 13, pos.x)
+	assert.Equal(t, 3, pos.y)
+
+	v = UpdateVelocity(v)
+	assert.Equal(t, 5, v.x)
+	assert.Equal(t, 0, v.y)
+
+	pos = ApplyVelocity(v, pos)
+	assert.Equal(t, 18, pos.x)
+	assert.Equal(t, 3, pos.y)
+}
+
+func Test_ShootFirstExample(t *testing.T) {
+	target := ParseTargetArea("target area: x=20..30, y=-10..-5")
+	v := Velocity{7, 2}
+	pos := Position{}
+	hit, _ := WillHit(pos, v, target)
+	assert.True(t, hit)
+
+	v = Velocity{6, 3}
+	pos = Position{}
+	hit, _ = WillHit(pos, v, target)
+	assert.True(t, hit)
+
+	v = Velocity{9, 0}
+	pos = Position{}
+	hit, _ = WillHit(pos, v, target)
+	assert.True(t, hit)
+
+	v = Velocity{17, -4}
+	pos = Position{}
+	hit, _ = WillHit(pos, v, target)
+	assert.False(t, hit)
+}
+
+func Test_FindMaxY(t *testing.T) {
+	target := ParseTargetArea("target area: x=20..30, y=-10..-5")
+
+	maxY := FindMaxY(target)
+
+	assert.Equal(t, 45, maxY)
+}
+
+func Test_FindMaxYDay17P1(t *testing.T) {
+	// Brute force best force! :D
+	target := ParseTargetArea("target area: x=288..330, y=-96..-50")
+
+	maxY := FindMaxY(target)
+
+	assert.Equal(t, 45, maxY)
+}
